f07bef972244ea5486ce489401da1377e8a44e33
Vašek Bayer <vasek.bayer@seznam.cz>
2016-03-21 11:56:01 +0100

    Revert "add 5.semestr"
    This reverts commit b2afbe875071a32728873f4bd2b6b1d84aff7a5e.

--- 5.semestr/IIS/hotel/www/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// let bootstrap create Dependency Injection container
-$container = require __DIR__ . '/../app/bootstrap.php';
-
-// run application
-$container->getService('application')->run();
